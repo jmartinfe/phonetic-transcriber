a36facfb35ee8c1b7eb64b380863555c7ff355be
Juan Martín <jmartinfe@gmail.com>
2026-03-11 17:50:42 -0600

    feat: compile notes separately from main dictionary

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
-from app.services.cmu_loader import compile_dictionary
 from app.core.logging_config import setup_logging
 
 setup_logging()
 
 if __name__ == "__main__":
-    compile_dictionary()
+    pass
--- app/services/cmu_loader.py
+++ app/services/cmu_loader.py
@@ -14,19 +14,20 @@ sources = {
 
 BUILD_DIR = Path("app/build") # Path to the folder where compiled files are stored
 ES_COMPILED_DICTIONARY_PATH = BUILD_DIR / "es_compiled_dictionary.json"  # Path to save the compiled dictionary JSON file
+ES_COMPILED_NOTES_PATH = BUILD_DIR / "es_compiled_notes.json"  # Path to save the compiled notes JSON file
 
 @dataclass
 class RuleMaps:
     transcriptions: dict
     ipa: dict
-    descriptions: dict
+    notes: dict
 
 def get_source_dict(source: Path) -> dict:
     """Returns dict structure based on source json file. """
     with source.open(encoding="utf-8") as f:
         return json.load(f)
 
-def needs_rebuild():
+def needs_rebuild_dictionary():
     """ Executes compiled dictionary rebuild if it doesn't exist or is not up to date """
     if not ES_COMPILED_DICTIONARY_PATH.exists():
         logger.info(f"Compiled dictionary not found at {ES_COMPILED_DICTIONARY_PATH}. Rebuild needed.")
@@ -41,37 +42,57 @@ def needs_rebuild():
     logger.info("Compiled dictionary is up to date. No rebuild needed.")
     return False
 
+def needs_rebuild_notes():
+    """ Executes compiled notes rebuild if it doesn't exist or is not up to date """
+    if not ES_COMPILED_NOTES_PATH.exists():
+        logger.info(f"Compiled notes not found at {ES_COMPILED_NOTES_PATH}. Rebuild needed.")
+        return True
+
+    compiled_time = ES_COMPILED_NOTES_PATH.stat().st_mtime
+    for s in sources.values():
+        if s.stat().st_mtime > compiled_time:
+            logger.info(f"Source file {s} has been modified since last compilation. Rebuild needed.")
+            return True
+
+    logger.info("Compiled notes are up to date. No rebuild needed.")
+    return False
+
 def parse_rules(transcriptions: dict) -> RuleMaps:
 
     trans_map = {}
     ipa_map = {}
-    desc_map = {}
+    notes_map = {}
     """
-        Build lookup maps for each phoneme rule so we can access transcription,
-        IPA symbol and description in O(1) time during dictionary compilation.
+        Build lookup maps for each phoneme rule so we can access transcription and
+        IPA symbol in O(1) time during dictionary compilation.
 
         Example rule:
         {
             "AA0": {
                 "transcription": "a",
-                "description": "normal pronunciation of Spanish a",
-                "ipa": "ɑ"
+                "ipa": "ɑ",
+                "note": "Sonido \"a\" como en \"cat\""
             }
         }
 
         Becomes three lookup tables:
         trans_map: {"AA0": "a"}
         ipa_map: {"AA0": "ɑ"}
-        desc_map: {"AA0": "/ɑ/: normal pronunciation of Spanish a"}
+        notes_map: {"AA0": "/ɑ/: Sonido \"a\" como en \"cat\""}
     """
     for phoneme, rule in transcriptions.items():
-        trans_map[phoneme] = rule.get("transcription", "")
+        transcription = rule.get("transcription", "")
+        trans_map[phoneme] = transcription
         ipa = rule.get("ipa", "")
         ipa_map[phoneme] = ipa
-        desc = rule.get("description", "")
-        desc_map[phoneme] = f"/{ipa}/: {desc}" if ipa and desc else "" # Only add to desc_map if there is a description
+        note = rule.get("note", "")
+        if note: # Only add to notes_map if there is a note
+            notes_map[transcription.upper()] = note
 
-    return RuleMaps(trans_map, ipa_map, desc_map)
+    for note in notes_map.values():
+        if note:
+            logger.debug(f"Note added: {note}")
+    return RuleMaps(trans_map, ipa_map, notes_map)
 
 def parse_pronunciations(rules: RuleMaps, cmu: dict) -> dict:
     """
@@ -88,8 +109,7 @@ def parse_pronunciations(rules: RuleMaps, cmu: dict) -> dict:
             "AN": [
                 {
                     "transcription": "an",
-                    "ipa": "ɑn",
-                    "description": "..."
+                    "ipa": "ɑn"
                 }
             ]
         }
@@ -109,29 +129,20 @@ def parse_pronunciations(rules: RuleMaps, cmu: dict) -> dict:
 
             transcription = []
             ipa = []
-            descriptions = []
-            seen = set()
 
-            # Build transcription, IPA and descriptions for the current pronunciation
+            # Build transcription and IPA for the current pronunciation
             # by iterating over its phonemes.
             for p in phonemes:
-
-                transcription.append(rules.transcriptions.get(p, ""))
+                transcription.append(
+                    "[" + rules.transcriptions.get(p, "").upper() + "]" if rules.notes.get(p, "")
+                    else rules.transcriptions.get(p, ""))
                 ipa.append(rules.ipa.get(p, ""))
-
-                desc = rules.descriptions.get(p)
-
-                if desc and desc not in seen:
-                    seen.add(desc)
-                    descriptions.append(desc)
 
             entry = {
                 "transcription": "".join(transcription),
                 "ipa": "".join(ipa)
             }
 
-            if descriptions:
-                entry["description"] = ". ".join(descriptions)
 
             compiled[base_word].append(entry)
 
@@ -139,17 +150,31 @@ def parse_pronunciations(rules: RuleMaps, cmu: dict) -> dict:
 
 def build_dictionary():
     rules_maps = parse_rules(get_source_dict(sources["es_transcriptions"]))
-    return parse_pronunciations(rules_maps, get_source_dict(sources["cmu"]))
+    return parse_pronunciations(rules_maps, get_source_dict(sources["cmu"])) 
 
 def compile_dictionary():
-    compiled = build_dictionary()
+    dictionary = build_dictionary()
     BUILD_DIR.mkdir(exist_ok=True)
     with ES_COMPILED_DICTIONARY_PATH.open("w", encoding="utf8") as f:
-        json.dump(compiled, f, ensure_ascii=False, separators=(",", ":"))
+        json.dump(dictionary, f, ensure_ascii=False, separators=(",", ":"))
+
+def compile_notes():
+    rules_maps = parse_rules(get_source_dict(sources["es_transcriptions"]))
+    notes = rules_maps.notes
+    BUILD_DIR.mkdir(exist_ok=True)
+    with ES_COMPILED_NOTES_PATH.open("w", encoding="utf8") as f:
+        json.dump(notes, f, ensure_ascii=False, separators=(",", ":"))
 
 @lru_cache
 def get_compiled_dictionary():
-    if needs_rebuild():
+    if needs_rebuild_dictionary():
         compile_dictionary()
     with ES_COMPILED_DICTIONARY_PATH.open(encoding="utf-8") as f:
+        return json.load(f)
+    
+@lru_cache
+def get_compiled_notes():
+    if needs_rebuild_notes():
+        compile_notes()
+    with ES_COMPILED_NOTES_PATH.open(encoding="utf-8") as f:
         return json.load(f)
